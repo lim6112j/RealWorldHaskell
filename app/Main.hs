2ea05385df320653cfa250adeb995f9ae541f196
lim6112j <lim6112j@gmail.com>
2021-08-15 15:25:38 +0900

    newtype, arrows, json parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 import Lib
-import PrettyJSON
-import Prettify
+-- import PrettyJSON
+-- import Prettify
+-- import NaiveEq
+-- import EqClasses
+import JSONClass
+import SimpleArrow
 import Prelude hiding ((<>))
 main :: IO ()
 main = putStrLn "hello"
